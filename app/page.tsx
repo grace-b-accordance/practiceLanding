64d7664a76421fca18d764ee8e0a0f0c9bea6af8
Grace B <gracebishara@accordance.ai>
2025-07-01 10:39:12 -0700

    working

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -100,4 +100,12 @@ export default function Home() {
       </footer>
     </div>
   );
+}
+
+export default function HOME() {
+return (
+  <div className="flex items-center justify-left min-h-screen bg-blue-500 text-white">
+    <h1 className="text-4xl font-bold">Tailwind is working!</h1>
+  </div>
+);
 }
